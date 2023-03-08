cfa5c6bd6239b974213b7b67f50174e112687fb2
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2023-03-07 23:29:05 -0300

    (feat): example route post with koajs configuration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import Koa from "koa";
+import cors from "koa-cors";
+import Router from "@koa/router";
+import bodyParser from "koa-bodyparser";
 
 const app = new Koa();
+
+const route = new Router();
+
+route.post("/user", (ctx, next) => {
+    console.log(ctx.request.body);
+    ctx.body = {
+        status: 200
+    }
+});
+
+app.use(cors());
+app.use(bodyParser());
+app.use(route.routes());
+
 
 app.listen(3000, () => {
     console.log("Server running at 3000 port!!");
